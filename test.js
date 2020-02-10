5bd519d2cd56b5a9f8c6d4cc7c3bac8587c27a38
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2020-02-10 01:02:29 +0100

    test: added tests

---
+++ test.js
@@ -0,0 +1,475 @@
+const fs = require('fs');
+const mock = require('mock-fs');
+const assert = require('assert');
+const recurse = require('fs-readdir-recursive');
+
+const build = require('.');
+
+suite('delete folder', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('missing', () => {
+		mock({
+			'empty-dir': {}
+		});
+
+		build.deleteFolder('nonexistent');
+		assert.equal(fs.existsSync('empty-dir'), true);
+	});
+
+	spec('empty folder', () => {
+		assert.equal(fs.existsSync('empty-dir'), false);
+
+		mock({
+			'empty-dir': {},
+			'other-dir': {}
+		});
+		assert.equal(fs.existsSync('empty-dir'), true);
+
+		build.deleteFolder('empty-dir');
+		assert.equal(fs.existsSync('empty-dir'), false);
+		assert.equal(fs.existsSync('other-dir'), true);
+	});
+
+	spec('full folder', () => {
+		assert.equal(fs.existsSync('empty-dir'), false);
+
+		mock({
+			'dir': {
+				'a': {},
+				'b': 'test',
+				'c': {
+					'd': 'test'
+				}
+			},
+			'empty-dir': {}
+		});
+		assert.equal(fs.existsSync('dir'), true);
+
+		build.deleteFolder('dir');
+		assert.equal(fs.existsSync('dir'), false);
+		assert.equal(fs.existsSync('empty-dir'), true);
+	});
+
+});
+
+
+suite('clean folder', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('missing', () => {
+		mock({
+			'empty-dir': {}
+		});
+
+		build.cleanFolder('nonexistent');
+		assert.equal(fs.existsSync('empty-dir'), true);
+		assert.equal(fs.existsSync('nonexistent'), true);
+	});
+
+	spec('empty folder', () => {
+		assert.equal(fs.existsSync('empty-dir'), false);
+
+		mock({
+			'empty-dir': {},
+			'other-dir': {}
+		});
+		assert.equal(fs.existsSync('empty-dir'), true);
+
+		build.cleanFolder('empty-dir');
+		assert.equal(fs.existsSync('empty-dir'), true);
+		assert.equal(fs.existsSync('other-dir'), true);
+	});
+
+	spec('full folder', () => {
+		assert.equal(fs.existsSync('empty-dir'), false);
+
+		mock({
+			'dir': {
+				'a': {},
+				'b': 'test',
+				'c': {
+					'd': 'test'
+				}
+			},
+			'empty-dir': {}
+		});
+		assert.equal(fs.existsSync('dir'), true);
+		assert.equal(fs.existsSync('dir/a'), true);
+
+		build.cleanFolder('dir');
+		assert.equal(fs.existsSync('dir'), true);
+		assert.equal(fs.existsSync('dir/a'), false);
+		assert.equal(fs.existsSync('empty-dir'), true);
+	});
+
+});
+
+
+suite('read', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('a', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		const res = build.read('a');
+		assert.equal(res, 'xyz');
+	});
+
+	spec('./a', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		const res = build.read('./a');
+		assert.equal(res, 'xyz');
+	});
+
+	spec('dir/b', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		const res = build.read('dir/b');
+		assert.equal(res, 'test');
+	});
+
+});
+
+
+suite('save', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.save('c', 'abc');
+		assert.equal(fs.readFileSync('a', 'utf-8'), 'xyz');
+		assert.equal(fs.readFileSync('dir/b', 'utf-8'), 'test');
+		assert.equal(fs.readFileSync('c', 'utf-8'), 'abc');
+	});
+
+	spec('./c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.save('./c', 'abc');
+		assert.equal(fs.readFileSync('a', 'utf-8'), 'xyz');
+		assert.equal(fs.readFileSync('dir/b', 'utf-8'), 'test');
+		assert.equal(fs.readFileSync('c', 'utf-8'), 'abc');
+	});
+
+	spec('dir/c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.save('dir/c', 'abc');
+		assert.equal(fs.readFileSync('a', 'utf-8'), 'xyz');
+		assert.equal(fs.readFileSync('dir/b', 'utf-8'), 'test');
+		assert.equal(fs.readFileSync('dir/c', 'utf-8'), 'abc');
+		assert.equal(fs.existsSync('c'), false);
+	});
+
+	spec('dir2/c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.save('dir2/c', 'abc');
+
+		assert.equal(fs.existsSync('c'), false);
+		assert.equal(fs.existsSync('dir2'), true);
+
+		assert.equal(fs.readFileSync('a', 'utf-8'), 'xyz');
+		assert.equal(fs.readFileSync('dir/b', 'utf-8'), 'test');
+		assert.equal(fs.readFileSync('dir2/c', 'utf-8'), 'abc');
+	});
+
+});
+
+
+suite('concat', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('concat', () => {
+		mock({
+			'a': '1\n',
+			'b': '2\n3',
+			'dir': {
+				'c': '4'
+			}
+		});
+
+		const res = build.concat(['a', 'b', 'dir/c']);
+		assert.equal(res, '1\n2\n34');
+	});
+
+});
+
+
+suite('create folders', {
+	eachSetup() {
+		mock.restore();
+	},
+// @ts-ignore
+}, () => {
+
+	spec('c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('c');
+		assert.equal(fs.existsSync('c'), false);
+	});
+
+	spec('c/', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('c/');
+		assert.equal(fs.existsSync('c'), true);
+	});
+
+	spec('dir/c', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('dir/c');
+		assert.equal(fs.existsSync('dir/c'), false);
+	});
+
+	spec('dir/c/', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('dir/c/');
+		assert.equal(fs.existsSync('dir/c'), true);
+	});
+
+	spec('c/d/e', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('c/d/e');
+		assert.equal(fs.existsSync('c'), true);
+		assert.equal(fs.existsSync('c/d'), true);
+		assert.equal(fs.existsSync('c/d/e'), false);
+	});
+
+	spec('c/d/e/', () => {
+		mock({
+			'a': 'xyz',
+			'dir': {
+				'b': 'test',
+			}
+		});
+
+		build.createFolders('c/d/e/');
+		assert.equal(fs.existsSync('c'), true);
+		assert.equal(fs.existsSync('c/d'), true);
+		assert.equal(fs.existsSync('c/d/e'), true);
+	});
+
+});
+
+
+suite('copy', {
+	eachSetup() {
+		mock.restore();
+		mock({
+			'a': 'file-a',
+			'assets': {
+				'b.png': 'file-b',
+				'c.png': 'file-c',
+				'd.png.gif': 'file-d',
+				'e': 'file-e'
+			}
+		});
+	},
+// @ts-ignore
+}, () => {
+
+	function getFiles() {
+		return recurse('.')
+			.map(s => s.replace(/\\/g, '/'));
+	}
+
+	spec('a -> dist/1/a', () => {
+		build.copy('a', 'dist/1/a');
+
+		const files = getFiles();
+		assert.deepEqual(files, ['a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e', 'dist/1/a']);
+		assert.equal(fs.readFileSync('dist/1/a', 'utf-8'), 'file-a');
+	});
+
+	spec('a -> dist/1/f', () => {
+		build.copy('a', 'dist/1/f');
+
+		const files = getFiles();
+		assert.deepEqual(files, ['a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e', 'dist/1/f']);
+		assert.equal(fs.readFileSync('dist/1/f', 'utf-8'), 'file-a');
+	});
+
+	spec('a -> dist/1/', () => {
+		build.copy('a', 'dist/1/');
+
+		const files = getFiles();
+		assert.deepEqual(files, ['a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e', 'dist/1/a']);
+		assert.equal(fs.readFileSync('dist/1/a', 'utf-8'), 'file-a');
+	});
+
+	spec('assets/ -> dist', () => {
+
+		try {
+			build.copy('assets/', 'dist');
+			assert.fail();
+		} catch (_) {}
+	});
+
+	spec('assets/ -> dist/', () => {
+		build.copy('assets/', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e',
+			'dist/b.png', 'dist/c.png', 'dist/d.png.gif', 'dist/e'
+		]);
+
+		assert.equal(fs.readFileSync('dist/b.png', 'utf-8'), 'file-b');
+		assert.equal(fs.readFileSync('dist/c.png', 'utf-8'), 'file-c');
+		assert.equal(fs.readFileSync('dist/d.png.gif', 'utf-8'), 'file-d');
+		assert.equal(fs.readFileSync('dist/e', 'utf-8'), 'file-e');
+	});
+
+	spec('assets/*.png -> dist/', () => {
+		build.copy('assets/*.png', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e',
+			'dist/b.png', 'dist/c.png'
+		]);
+
+		assert.equal(fs.readFileSync('dist/b.png', 'utf-8'), 'file-b');
+		assert.equal(fs.readFileSync('dist/c.png', 'utf-8'), 'file-c');
+	});
+
+	spec('assets/? -> dist/', () => {
+		build.copy('assets/?', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e',
+			'dist/e'
+		]);
+
+		assert.equal(fs.readFileSync('dist/e', 'utf-8'), 'file-e');
+	});
+
+	spec('assets/g* -> dist/', () => {
+		build.copy('assets/g*', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e'
+		]);
+	});
+
+	spec('assets/?* -> dist/', () => {
+		build.copy('assets/?*', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e',
+			'dist/b.png', 'dist/c.png', 'dist/d.png.gif', 'dist/e'
+		]);
+
+		assert.equal(fs.readFileSync('dist/b.png', 'utf-8'), 'file-b');
+		assert.equal(fs.readFileSync('dist/c.png', 'utf-8'), 'file-c');
+		assert.equal(fs.readFileSync('dist/d.png.gif', 'utf-8'), 'file-d');
+		assert.equal(fs.readFileSync('dist/e', 'utf-8'), 'file-e');
+	});
+
+	spec('assets/*? -> dist/', () => {
+		build.copy('assets/*?', 'dist/');
+
+		const files = getFiles();
+		assert.deepEqual(files, [
+			'a', 'assets/b.png', 'assets/c.png', 'assets/d.png.gif', 'assets/e',
+			'dist/b.png', 'dist/c.png', 'dist/d.png.gif', 'dist/e'
+		]);
+
+		assert.equal(fs.readFileSync('dist/b.png', 'utf-8'), 'file-b');
+		assert.equal(fs.readFileSync('dist/c.png', 'utf-8'), 'file-c');
+		assert.equal(fs.readFileSync('dist/d.png.gif', 'utf-8'), 'file-d');
+		assert.equal(fs.readFileSync('dist/e', 'utf-8'), 'file-e');
+	});
+
+});
